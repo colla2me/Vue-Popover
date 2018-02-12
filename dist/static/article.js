391dd3cd68f283ed3fab18653d97cfcdf924e602
zhanglevan <zhanglevan@gmail.com>
2018-02-12 22:10:20 +0800

    popover component

---
+++ dist/static/article.js
@@ -0,0 +1,101 @@
+export default {
+  "code": 20000,
+  "message": "OK",
+  "data": {
+    "us": [{
+      "author": {
+        "avatar": "https://wdl.wallstreetcn.com/f83fa94f-b838-4b2b-8b1f-eb0115d7a159",
+        "display_name": "张丹丹",
+        "id": 120000000282,
+        "uri": "https://wallstreetcn.com/authors/120000000282"
+      },
+      "comment_count": 0,
+      "content_short": "“如果你是美股多头，下周一是个大日子”。",
+      "display_time": 1515329688,
+      "id": 3054212,
+      "image_uri": "https://wpimg.wallstcn.com/f3f2bc64-c71f-4b72-a604-73fc52be5aa5.jpg",
+      "title": "十二年来最佳开局 对美股意味着什么？",
+      "uri": "https://wallstreetcn.com/articles/3054212"
+    }, {
+      "author": {
+        "avatar": "https://dn-wscn-avatar.qbox.me/28/62/7f/dsc-2419.jpg",
+        "display_name": "陶旖洁",
+        "id": 100001377409,
+        "uri": "https://wallstreetcn.com/authors/100001377409"
+      },
+      "comment_count": 0,
+      "content_short": "中美都将在下周发布CPI数据；多名拥有投票权的联储高官都将发表讲话，包括三号人物、纽约联储主席杜德利，关注对税改以及低通胀的观点；朝韩将于周二进行正式会谈。",
+      "display_time": 1515316087,
+      "id": 3053849,
+      "image_uri": "https://wpimg.wallstcn.com/e2d17665-41b8-4c15-9ca9-ae9d71fa6eef.jpg",
+      "title": "下周重磅财经事件一览：中美通胀数据、美联储高官讲话、朝韩会谈",
+      "unshow_content_short": false,
+      "uri": "https://wallstreetcn.com/premium/articles/3053849?layout=wscn-layout"
+    }],
+    "china": [{
+      "author": {
+        "avatar": "https://wpimg.wallstcn.com/1761722f-7839-4306-b322-a14630ac2e7f",
+        "display_name": "脱水研报",
+        "id": 120000000496,
+        "uri": "https://wallstreetcn.com/authors/120000000496"
+      },
+      "categories": ["global", "china", "economy", "enterprise", "shares"],
+      "comment_count": 0,
+      "content_short": "今日内容：1、继新能源后汽车的下一个爆点，周末多家券商电话轰炸此主题；2、选周期还是选消费，2018年你要抱紧谁的大腿？3、当产能扩增遇上行业高景气度，这个有机硅龙头已经被资金盯上了；4、航线放开、票价上涨实锤落定，航空股的业绩弹性顺序都给你标好了；5、本月中下旬补库存+国内外价差修复，一品种价格已经憋不住了。",
+      "display_time": 1515329612,
+      "id": 3054221,
+      "image_uri": "https://wpimg.wallstcn.com/49029804-0392-4247-b1f2-cfc610f11911.jpg",
+      "title": "继新能源后汽车的下一个爆点，周末多家券商电话轰炸此主题；周期VS消费，2018年你该抱紧谁的大腿——1月7日A股脱水研报",
+      "unshow_content_short": false,
+      "uri": "https://wallstreetcn.com/premium/articles/3054221?layout=wscn-layout"
+    }, {
+      "author": {
+        "avatar": "https://wpimg.wallstcn.com/4c4b2c0c-5889-4525-82e7-ad9f77a898bd",
+        "display_name": "选股宝早知道",
+        "id": 120000001226,
+        "uri": "https://wallstreetcn.com/authors/120000001226"
+      },
+      "categories": ["china", "enterprise", "shares", "economy"],
+      "comment_count": 0,
+      "content_short": "航空运输价格改革重要文件落地，这一板块利润天花板打开；发改委文件透露重要信号，这个热点主题再添重大催化剂；十二部委联手发布重磅文件，2018年这类行业的机会将从“涨价”转向“重组”",
+      "display_time": 1515327973,
+      "id": 3054230,
+      "image_uri": "https://wpimg.wallstcn.com/44746091-d7d1-437a-9457-e5515ae51c67.jpg",
+      "title": "多个城市释放限购松动信号，这类A股公司2018年望逆袭；国家最高科技大奖8日揭晓，这份A股名单必须收好 | 1月8日A股主题早知道",
+      "unshow_content_short": false,
+      "uri": "https://wallstreetcn.com/premium/articles/3054230?layout=wscn-layout"
+    }],
+    "europe": [{
+      "author": {
+        "avatar": "https://dn-wscn-avatar.qbox.me/28/62/7f/dsc-2419.jpg",
+        "display_name": "陶旖洁",
+        "id": 100001377409,
+        "uri": "https://wallstreetcn.com/authors/100001377409"
+      },
+      "comment_count": 0,
+      "content_short": "中美都将在下周发布CPI数据；多名拥有投票权的联储高官都将发表讲话，包括三号人物、纽约联储主席杜德利，关注对税改以及低通胀的观点；朝韩将于周二进行正式会谈。",
+      "display_time": 1515310267,
+      "external": false,
+      "id": 3053849,
+      "image_uri": "https://wpimg.wallstcn.com/e2d17665-41b8-4c15-9ca9-ae9d71fa6eef.jpg",
+      "title": "下周重磅财经事件一览：中美通胀数据、美联储高官讲话、朝韩会谈",
+      "uri": "https://wallstreetcn.com/premium/articles/3053849?layout=wscn-layout"
+    }, {
+      "author": {
+        "avatar": "https://dn-wscn-avatar.qbox.me//61/80/94/-logo.png",
+        "display_name": "华尔街见闻",
+        "id": 109176,
+        "uri": "https://wallstreetcn.com/authors/109176"
+      },
+      "comment_count": 0,
+      "content_short": "我们每天读几十万字，只为你5分钟的阅读时间。文末有彩蛋～",
+      "display_time": 1515285036,
+      "external": false,
+      "id": 3053875,
+      "image_uri": "https://wpimg.wallstcn.com/0ad7fc0f-334e-40a6-be3c-48bcc1c83aff.jpg",
+      "title": "一周全球干货梳理：金融监管“最强风暴”、比特币“造富神话”、微信支付宝们的“隐忧”",
+      "unshow_content_short": false,
+      "uri": "https://wallstreetcn.com/articles/3053875"
+    }]
+  }
+}
---
+++ dist/static/data.js
@@ -0,0 +1,72 @@
+export default {
+  id: 0,
+  label: "XXX科技有限公司",
+  expand: true,
+  children: [{
+      id: 2,
+      label: "产品研发部",
+      expand: true,
+      children: [{
+          id: 5,
+          label: "研发-前端",
+          // children: [{
+          //     id: 11,
+          //     label: "移动端-iOS"
+          //   },
+          //   {
+          //     id: 12,
+          //     label: "移动端-Android"
+          //   },
+          //   {
+          //     id: 13,
+          //     label: "PC软件"
+          //   }
+          // ]
+        },
+        {
+          id: 6,
+          label: "研发-后端"
+        },
+        {
+          id: 9,
+          label: "UI设计"
+        },
+        {
+          id: 10,
+          label: "产品经理"
+        }
+      ]
+    },
+    {
+      id: 3,
+      label: "销售部",
+      expand: true,
+      children: [{
+          id: 7,
+          label: "销售一部"
+        },
+        {
+          id: 8,
+          label: "销售二部"
+        }
+      ]
+    },
+    {
+      id: 4,
+      label: "财务部"
+    },
+    {
+      id: 9,
+      label: "HR人事",
+      // children: [{
+      //     id: 14,
+      //     label: '人事经理'
+      //   },
+      //   {
+      //     id: 15,
+      //     label: "人事专员"
+      //   }
+      // ]
+    }
+  ]
+}
